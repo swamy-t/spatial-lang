551142b2a6e60ebc709f75097910b173152a051f
raghu <raghup17@gmail.com>
2017-07-07 11:16:48 -0700

    Modify scripts to run baseline and n3xt simulations

--- bin/vulture.sh
+++ bin/vulture.sh
@@ -14,21 +14,35 @@ screen -d -m -S $SESSION_NAME
 # For each subdirectory in the current directory, create a new screen window and launch job
 for f in $PWD/*; do
   if [ -d $f ]; then
-    CMD="cd $f; bash cmd"
+    for idealDRAM in $(seq 0 1); do
+      SUFFIX="unk"
 
-    # Creates a new screen window with title '$f' in existing screen session
-    screen -S $SESSION_NAME -X screen -t $f
+      if [ $idealDRAM -eq 0 ]; then
+        SUFFIX="baseline"
+      else
+        SUFFIX="n3xt"
+      fi
 
-    # Switch terminal to bash
-    screen -S $SESSION_NAME -p $f -X stuff "bash$(printf \\r)"
-    
-    # Launch $CMD in newly created screen window
-    screen -S $SESSION_NAME -p $f -X stuff "$CMD$(printf \\r)"
+      CMD="cd $f; USE_IDEAL_DRAM=$idealDRAM bash run.sh $(cat args) | tee ${SUFFIX}.log"
 
-    # Acknowledge the screen exists and log if it doesn't
-    # logger "something"
-    
-    # Sleep because this may fix clobbering bug
-    sleep 0.2
+      # Window title for session to be created
+      WINDOW_TITLE=${f}_$SUFFIX
+
+      # Creates a new screen window with the above title in existing screen session
+      screen -S $SESSION_NAME -X screen -t $WINDOW_TITLE
+
+      # Switch terminal to bash
+      screen -S $SESSION_NAME -p $WINDOW_TITLE -X stuff "bash$(printf \\r)"
+
+      # Launch $CMD in newly created screen window
+      screen -S $SESSION_NAME -p $WINDOW_TITLE -X stuff "$CMD$(printf \\r)"
+
+      # Acknowledge the screen exists and log if it doesn't
+      # logger "something"
+
+      # Sleep because this may fix clobbering bug
+      sleep 0.2
+
+    done
   fi
 done
